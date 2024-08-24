7783cde1fd8e1382f150f6cd5bf3a338bf78aa9e
Edu In Thai <k.avadakedavra@gmail.com>
2024-08-24 18:54:45 +0700

    Create script.js

---
+++ prasuk/pp5/script.js
@@ -0,0 +1,22 @@
+document.getElementById('login-form').addEventListener('submit', function(e) {
+    e.preventDefault();  // ป้องกันการรีเฟรชหน้าหลังจากกดปุ่ม Submit
+
+    var inputPassword = document.getElementById('password').value;
+
+    // ดึงข้อมูลจาก Google Sheets
+    fetch('https://script.google.com/macros/s/AKfycbymITyEcnX2fWibMRJ9CYcEFQ1KWUlmpZsq99dYd1ep9SNLSMiRZy7SjNKyv37xt5CJiA/exec')
+        .then(response => response.json())
+        .then(data => {
+            var sheetPassword = data.password;
+
+            // ตรวจสอบรหัสผ่าน
+            if(inputPassword === sheetPassword) {
+                alert("Login successful!");
+                document.querySelector('.login-container').style.display = 'none';  // ซ่อนฟอร์มล็อกอิน
+                document.getElementById('iframe-container').style.display = 'block';  // แสดง iframe
+            } else {
+                alert("Invalid password!");  // แจ้งเตือนหากรหัสผ่านไม่ถูกต้อง
+            }
+        })
+        .catch(error => console.error('Error:', error));
+});
